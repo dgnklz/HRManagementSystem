a90c8133e0d48703142a73039d7be6b96a259364
dgnklz <dogankilaz@gmail.com>
2024-01-29 21:23:18 +0300

    modelmapper

--- src/main/java/com/dgnklz/hrmanagementsystem/business/conceretes/DepartmentManager.java
+++ src/main/java/com/dgnklz/hrmanagementsystem/business/conceretes/DepartmentManager.java
@@ -3,9 +3,11 @@ package com.dgnklz.hrmanagementsystem.business.conceretes;
 import com.dgnklz.hrmanagementsystem.business.abstracts.DepartmentService;
 import com.dgnklz.hrmanagementsystem.business.dto.requests.department.CreateDepartmentRequest;
 import com.dgnklz.hrmanagementsystem.business.dto.responses.CreateDepartmentResponse;
+import com.dgnklz.hrmanagementsystem.core.mapping.ModelMapperService;
 import com.dgnklz.hrmanagementsystem.entity.Department;
 import com.dgnklz.hrmanagementsystem.repository.DepartmentRepository;
 import lombok.AllArgsConstructor;
+import org.hibernate.mapping.DependantBasicValue;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -14,19 +16,35 @@ import org.springframework.stereotype.Service;
 public class DepartmentManager implements DepartmentService {
     @Autowired
     private DepartmentRepository repository;
+    private ModelMapperService mapper;
 
     @Override
     public CreateDepartmentResponse add(CreateDepartmentRequest request) {
-        Department department = new Department();
+        try {
+            checkIfDepartmentExistByName(request.getDepartmentName());
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+        /*Department department = new Department();
         department.setDepartmentName(request.getDepartmentName());
-        department.setDescription(request.getDescription());
+        department.setDescription(request.getDescription());*/
+        Department department = mapper.forRequest().map(request, Department.class);
 
         repository.save(department);
 
-        CreateDepartmentResponse response = new CreateDepartmentResponse();
+        /*CreateDepartmentResponse response = new CreateDepartmentResponse();
         response.setDepartmentName(department.getDepartmentName());
-        response.setDescription(department.getDescription());
+        response.setDescription(department.getDescription());*/
+        CreateDepartmentResponse response = mapper.forResponse().map(department, CreateDepartmentResponse.class);
 
         return response;
+    }
+
+    /// DOMAIN RULES \\\
+
+    private void checkIfDepartmentExistByName(String name) throws Exception {
+        if(repository.existsByDepartmentName(name)){
+            throw new Exception("Department exist");
+        }
     }
 }
