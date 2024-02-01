e6b0f769b2672d7bcacfbc69cb0481e1b694df2d
vlalli <viniciuslalli@gmail.com>
2024-02-01 20:05:22 -0300

    - EmployeeManager + EmployeeRepository + EmployeeService + Employe response and requests

---
+++ src/main/java/com/dgnklz/hrmanagementsystem/business/abstracts/EmployeeService.java
@@ -0,0 +1,15 @@
+package com.dgnklz.hrmanagementsystem.business.abstracts;
+
+import com.dgnklz.hrmanagementsystem.business.dto.requests.employee.CreateEmployeeRequest;
+import com.dgnklz.hrmanagementsystem.business.dto.responses.department.CreateDepartmentResponse;
+import com.dgnklz.hrmanagementsystem.business.dto.responses.employee.CreateEmployeeResponse;
+import com.dgnklz.hrmanagementsystem.business.dto.responses.employee.GetAllEmployeeResponse;
+import com.dgnklz.hrmanagementsystem.core.result.DataResult;
+
+import java.util.List;
+
+public interface EmployeeService {
+
+    DataResult<CreateEmployeeResponse> add(CreateEmployeeRequest request);
+    DataResult<List<GetAllEmployeeResponse>> getAll(GetAllEmployeeResponse request);
+}
